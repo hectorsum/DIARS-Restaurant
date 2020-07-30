06c4bad5b7c2ae7705a7f235931fae438ea1c9bf
Hector Herrera <hectorsum81@hotmail.com>
2020-07-30 12:00:03 -0500

    routes, middlewares, connect-flash added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const morgan = require('morgan');
 const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const mysqlstore = require('express-mysql-session');
+const {database} = require('./key');
+const { allowedNodeEnvironmentFlags } = require('process');
+const passport = require('passport');
+
 
 //Initializations
 const app = express();
+require('./lib/passport');
 
 //Settings 
 app.set('port',process.env.PORT || 4000);
@@ -22,12 +30,31 @@ app.engine('.hbs',exphbs({
 app.set('view options',{layout:'index'})
 
 //Middlewares
+app.use(session({
+  secret:'343ji43j4n3jn4jk3n',
+  resave:false,
+  saveUninitialized:false,
+  store:new mysqlstore(database)
+}));
+app.use(flash());
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
+//Global variables
+app.use((req,res,next)=>{
+  app.locals.success = req.flash('success');
+  app.locals.failure = req.flash('failure');
+  app.locals.user = req.user;
+  next();
+})
 
 //Routes
 app.use(require('./routes/index'));
 app.use(require('./routes/auth'));
+app.use(require('./routes/home'));
 
 //Public
 app.use(express.static(path.join(__dirname,'public')));
