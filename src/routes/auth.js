07f1dbea6bb51644891c455f2d70177a7f0c9c86
Hector Herrera <hectorsum81@hotmail.com>
2020-07-30 12:02:05 -0500

    POST method added for verifying user login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/signin', (req,res)=>{
+router.get('/signin', async(req,res)=>{
   res.render('auth/signin')
 });
+
+router.post('/signin',(req,res,next)=>{
+  passport.authenticate('local.signin',{
+    successRedirect:'/home',
+    failureRedirect:'/signin',
+    failureFlash:true
+  })(req,res,next);
+})
 
 module.exports = router;
