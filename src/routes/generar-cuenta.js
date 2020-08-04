33ddb69037b6110cb6fc45c7417888be15dcae6f
Hector Herrera <hectorsum81@hotmail.com>
2020-08-03 20:42:28 -0500

    get method was updated

--- src/routes/generar-cuenta.js
+++ src/routes/generar-cuenta.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const pool = require('../database');
 const router = express.Router();
 const {islogedin,isnotlogedin} = require('../lib/out');
 
-router.get('/generar-cuenta',isnotlogedin,(req,res)=>{
-  res.render('generar-cuenta/generar-cuenta')
+router.get('/generar-cuenta',isnotlogedin,async(req,res)=>{
+  const venta = await pool.query('SELECT * FROM venta')
+  res.render('generar-cuenta/generar-cuenta',{venta})
 })
+
+// router.post('/generar-cuenta', async(req,res)=>{
+
+// })
 
 module.exports = router;
