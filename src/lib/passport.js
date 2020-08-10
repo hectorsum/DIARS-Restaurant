086632b2bebe571af15924fb64b9a926d7830a8d
Hector Herrera <hectorsum81@hotmail.com>
2020-08-09 20:26:11 -0500

    new queries replaced for getting user`s data

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -8,11 +8,12 @@ passport.use('local.signin', new localstrategy({
   passportField:'password',
   passReqToCallback: true
 }, async(req,username,password,done)=>{
-  const rows = await pool.query('SELECT * FROM usuario_emp WHERE usuario = ?',[username]);
+  const rows = await pool.query("SELECT `usuario_emp`.*, `empleado`.`nombres`, `empleado`.`apellidos` FROM `usuario_emp` LEFT JOIN `empleado` ON `usuario_emp`.`cod_emp` = `empleado`.`cod_emp` WHERE `usuario_emp`.`usuario` = ?",[username]);
   if(rows.length > 0){
     const user = rows[0];
+    console.log(user)
     if(user.password === password){
-      return done(null,user,req.flash('success','Welcome'));
+      return done(null,user,req.flash('success',`Welcome ${user.nombres} ${user.apellidos}`));
     }else{
       return done(null,false,req.flash('failure','Incorrect password'));
     }
@@ -21,11 +22,13 @@ passport.use('local.signin', new localstrategy({
   }
 }));
 
+//* Allows calling user.<column> in partials
 passport.serializeUser((user,done)=>{
   done(null,user.cod_usuario_emp)
 })
-
+//* Allows us to use empleados columns with user.<nombres>
 passport.deserializeUser(async(id,done)=>{
-  const rows = await pool.query('SELECT * FROM usuario_emp WHERE cod_usuario_emp = ?',[id]);
+  const rows = await pool.query("SELECT `usuario_emp`.*, `empleado`.`nombres`, `empleado`.`apellidos` FROM `usuario_emp` LEFT JOIN `empleado` ON `usuario_emp`.`cod_emp` = `empleado`.`cod_emp` WHERE `usuario_emp`.`cod_usuario_emp` = ?",[id]);
+  
   done(null,rows[0]);
 })
