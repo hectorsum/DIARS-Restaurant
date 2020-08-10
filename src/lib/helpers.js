9e368b03397156ad95755b91f1e01dff334e4cd5
Hector Herrera <hectorsum81@hotmail.com>
2020-08-09 20:25:00 -0500

    encrypt decrypt helpers added

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,11 +1,18 @@
 const moment = require('moment');
 const numeral = require('numeral');
+const bcrypt = require('bcryptjs');
 const helpers = {};
 
 helpers.index = (index) =>{
   index++
   return index;
 }
+
+helpers.select = ('select', function(selected, options) {
+  return options.fn(this).replace(
+      new RegExp(' value=\"' + selected + '\"'),
+      '$& selected="selected"');
+});
 
 helpers.formatdaytime = (time)=>{
   return moment(time).format('DD/MM/YYYY HH:mm:ss')
@@ -15,6 +22,26 @@ helpers.money= (number) =>{
   var number = numeral(number);
   const money ='S/'+number.format('0,0.00')
   return money;
+}
+
+helpers.encryptpassword = async(password) =>{
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password,salt);
+  return hash;
+}
+
+helpers.decryptpassword = async(password)=>{
+  const salt = await bcrypt.genSalt(10);
+  const hash = bcrypt.decodeBase64(password);
+  return hash;
+}
+
+helpers.matchpassword = async(password,savedpassword) =>{
+  try{
+    return await bcrypt.compare(password,savedpassword);
+  }catch(e){
+    return false;
+  }
 }
 
 module.exports = helpers;
