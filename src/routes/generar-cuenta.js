83fe359a5bf17b53c04e1ec4039fa7c5a9274625
Hector Herrera <hectorsum81@hotmail.com>
2020-08-11 20:11:19 -0500

    edit action from generar-cuenta added

--- src/routes/generar-cuenta.js
+++ src/routes/generar-cuenta.js
@@ -4,10 +4,10 @@ const helpers = require('../lib/helpers');
 const router = express.Router();
 const {islogedin,isnotlogedin} = require('../lib/out');
 
-router.get('/generar-cuenta',isnotlogedin,async(req,res)=>{
+router.get('/',isnotlogedin,async(req,res)=>{
   
   //*Querying tables
-  const venta = await pool.query("SELECT `venta`.*, `tipo_pago`.`nombre` FROM `venta` LEFT JOIN `tipo_pago` ON `venta`.`cod_tipo_pago` = `tipo_pago`.`cod_tipo_pago`")
+  const venta = await pool.query("SELECT `venta`.*, `tipo_pago`.`nombre` FROM `venta` LEFT JOIN `tipo_pago` ON `venta`.`cod_tipo_pago` = `tipo_pago`.`cod_tipo_pago` ORDER BY `venta`.`cod_ven` DESC")
   const entrada = await pool.query('SELECT * FROM entrada')
   const segundo = await pool.query('SELECT * FROM segundo')
   const producto = await pool.query('SELECT * FROM producto')
@@ -17,28 +17,33 @@ router.get('/generar-cuenta',isnotlogedin,async(req,res)=>{
   res.render('generar-cuenta/generar-cuenta',{venta,entrada,segundo,producto,empleado,tipo_pago,usuario_emp})
 })
 
-router.post('/generar-cuenta', async(req,res)=>{
+router.post('/', async(req,res)=>{
   const {nombre_segundo,nombre_entrada,nombre_producto,cod_tipo_pago,cant_entrada,cant_segundo,cant_producto} = req.body;
 
-  console.log(nombre_segundo)
+  const nombre_producto_string = await pool.query(`SELECT nombre_producto FROM producto WHERE cod_prod=?`,[nombre_producto])
+  const nombre_entrada_string = await pool.query(`SELECT nombre_entrada FROM entrada WHERE cod_entrada=?`,[nombre_entrada])
+  const nombre_segundo_string = await pool.query(`SELECT nombre_segundo FROM segundo WHERE cod_segundo=?`,[nombre_segundo])
+  const nombre_producto_extracted = nombre_producto_string[0].nombre_producto;
+  const nombre_entrada_extracted = nombre_entrada_string[0].nombre_entrada
+  const nombre_segundo_extracted = nombre_segundo_string[0].nombre_segundo
+  
   //*Getting timestamp
   const current_timestamp = await pool.query('SELECT CURRENT_TIMESTAMP');
   const fecha_venta = current_timestamp[0].CURRENT_TIMESTAMP
 
-  //*Getting payment state
-  const estado_pago = (cod_tipo_pago===2) ? 'Cancelado':'Pendiente';
   //*Getting prices
-  const entrada_precio = await pool.query(`SELECT precio FROM entrada WHERE cod_entrada LIKE '${nombre_entrada}'`)
-  const segundo_precio = await pool.query(`SELECT precio FROM segundo WHERE cod_segundo LIKE '${nombre_segundo}'`)
-  const producto_precio = await pool.query(`SELECT precio FROM producto WHERE cod_prod LIKE '${nombre_producto}'`)
-
+  const entrada_precio = await pool.query(`SELECT precio FROM entrada WHERE nombre_entrada=?`,[nombre_entrada_extracted]);
+  const segundo_precio = await pool.query(`SELECT precio FROM segundo WHERE nombre_segundo=?`,[nombre_segundo_extracted]);
+  const producto_precio = await pool.query(`SELECT precio FROM producto WHERE nombre_producto=?`,[nombre_producto_extracted]);
+  console.log(entrada_precio)
+  console.log(segundo_precio)
+  console.log(producto_precio)
   const valor = (cant_entrada*entrada_precio[0].precio)+(cant_segundo*segundo_precio[0].precio)+
   (cant_producto*producto_precio[0].precio);
   const igv = 0.18;
   const descuento = valor*igv;
   const monto_total = valor-descuento;
-
-  await pool.query('call insert_venta_local(?,?,?,?,?,?,?,?,?,?)',[nombre_segundo,nombre_entrada,nombre_producto,valor,monto_total,fecha_venta,cant_entrada,cant_segundo,cant_producto,cod_tipo_pago],async(err,resp,fields)=>{
+  await pool.query('call insert_venta_local(?,?,?,?,?,?,?,?,?,?)',[nombre_segundo_extracted,nombre_entrada_extracted,nombre_producto_extracted,valor,monto_total,fecha_venta,cant_entrada,cant_segundo,cant_producto,cod_tipo_pago],async(err,resp,fields)=>{
     if (err) {
       console.log(err)
       req.flash('failure', "Couldn't register" + err);
@@ -51,5 +56,15 @@ router.post('/generar-cuenta', async(req,res)=>{
     }
   });
 });
+
+router.get('/edit/:cod_ven',async(req,res)=>{
+  const {cod_ven} = req.params;
+  const venta = pool.query('SELECT * FROM venta WHERE cod_ven=?',[cod_ven])
+  res.render('generar-cuenta/edit',venta)
+})
+router.post('/edit/:cod_ven',async(req,res)=>{
+  
+})
+
 
 module.exports = router;
