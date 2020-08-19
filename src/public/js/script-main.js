f304970dee3403fc6f1d5c7ba0486bbfa4763404
Hector Herrera <hectorsum81@hotmail.com>
2020-08-18 21:03:06 -0500

    "0" number added in time and some functions for "cantidades" as well

--- src/public/js/script-main.js
+++ src/public/js/script-main.js
@@ -34,6 +34,7 @@ try {
 //todo: Setting time and date
 // window.onload() = setInterval(clock,1000);
 window.addEventListener('load',setInterval(clock,1000));
+
 function clock(){
   var date = new Date();
   var day = date.getDate();
@@ -42,5 +43,71 @@ function clock(){
   var hour = date.getHours();
   var min = date.getMinutes();
   var sec = date.getSeconds();
-  document.getElementById('date').placeholder = `${day}/${month+1}/${year} ${hour}:${min}:${sec}`
+  
+  //*Validations date
+  var sec = (sec<10) ? `0${sec}` : sec;
+  var min = (min<10) ? `0${min}` : min;
+  var hour = (hour<10) ? `0${hour}`: hour;
+  var month = (month<10) ? `0${month+1}` : month+1;
+
+  const time = document.getElementById('date').placeholder = `${day}/${month}/${year} ${hour}:${min}:${sec}`
+  return time;
 }
+
+function changedSegundo(){
+  const nombre_segundo = document.getElementById('nombre_segundo');
+  //* Getting integer value from selected tag
+  const opc_nombre_segundo = nombre_segundo.options[nombre_segundo.selectedIndex].value;
+  const cant_segundo = document.getElementById('cant_segundo');
+
+  switch(opc_nombre_segundo){
+    case '0':
+      cant_segundo.setAttribute('readonly',true);
+      cant_segundo.setAttribute('value','');
+      break;
+    default:
+      cant_segundo.removeAttribute('readonly');
+      cant_segundo.setAttribute('value','1');
+      break;
+  }
+}
+
+function changedEntrada(){
+  const nombre_entrada = document.getElementById('nombre_entrada');
+  //* Getting integer value from selected tag
+  const opc_nombre_entrada = nombre_entrada.options[nombre_entrada.selectedIndex].value;
+  const cant_entrada = document.getElementById('cant_entrada');
+
+  switch(opc_nombre_entrada){
+    case '0':
+      cant_entrada.setAttribute('readonly',true);
+      cant_entrada.setAttribute('value','');
+      break;
+    default:
+      cant_entrada.removeAttribute('readonly');
+      cant_entrada.setAttribute('value','1');
+      break;
+  }
+}
+
+function changedProducto(){
+  const nombre_producto = document.getElementById('nombre_producto');
+  //* Getting integer value from selected tag
+  const opc_nombre_producto = nombre_producto.options[nombre_producto.selectedIndex].value;
+  const cant_producto = document.getElementById('cant_producto');
+
+  switch(opc_nombre_producto){
+    case '0':
+      cant_producto.setAttribute('readonly',true);
+      cant_producto.setAttribute('value','');
+      break;
+    default:
+      cant_producto.removeAttribute('readonly');
+      cant_producto.setAttribute('value','1');
+      break;
+  }
+}
+
+window.addEventListener('change',changedSegundo);
+window.addEventListener('change',changedEntrada);
+window.addEventListener('change',changedProducto);
