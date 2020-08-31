79e3ac4b3d0e29d59e8dc45aeb5b6574a0e06da5
Hector Herrera <hectorsum81@hotmail.com>
2020-08-30 22:50:58 -0500

    mantener-entrada route added

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ app.use((req,res,next)=>{
 //*Specifying route bc we're using edit,delete in there
 app.use('/generar-cuenta',require('./routes/generar-cuenta'));
 app.use('/mantener-producto',require('./routes/mantener-producto'));
+app.use('/mantener-entrada',require('./routes/mantener-entrada'));
 app.use(require('./routes/mantener-producto'));
 app.use(require('./routes/index'));
 app.use(require('./routes/auth'));
