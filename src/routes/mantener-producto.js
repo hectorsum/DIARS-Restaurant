33ea14305b47224d1cf88e33f9bdda3bf17d4637
Hector Herrera <hectorsum81@hotmail.com>
2020-08-30 22:52:41 -0500

    unused helper importation removed

--- src/routes/mantener-producto.js
+++ src/routes/mantener-producto.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const pool = require('../database');
-const helpers = require('../lib/helpers');
 const router = express.Router();
 const {islogedin,isnotlogedin}=require('../lib/out');
 
