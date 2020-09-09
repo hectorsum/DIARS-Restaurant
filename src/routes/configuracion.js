91d8c70d2171ffb8d718f50559a22c2d69eafd32
Hector Herrera <hectorsum81@hotmail.com>
2020-09-08 23:49:23 -0500

    POST method added

--- src/routes/configuracion.js
+++ src/routes/configuracion.js
@@ -1,10 +1,22 @@
 const pool = require('../database');
 const {isnotlogedin} = require('../lib/out');
 const express = require('express');
+const helpers = require('../lib/helpers');
 const router = express.Router();
+const jsdom = require("jsdom");
+const d3 = require("d3");
+
 
 router.get('/configuracion',isnotlogedin,async(req,res)=>{
-  res.render('configuracion/configuracion');
+  const ubdepartamento	 = pool.query('SELECT * FROM ubdepartamento');
+  res.render('configuracion/configuracion',{ubdepartamento});
 });
 
+router.post('/configuracion',isnotlogedin,async(req,res)=>{
+  const { nombre_restaurant,direccion,email,departamento,provincia,distrito } = req.body;
+  jsdom.env(window.location.href,function(err,windows){
+    if (error) throw error;
+    console.log(d3.select(windows.document).select("#seleccionar-departamento"));
+  })
+});
 module.exports = router;
