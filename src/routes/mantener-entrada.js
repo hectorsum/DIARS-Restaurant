11fbcf21d4dea9f5d276a9c418dd7af93717297e
Hector Herrera <hectorsum81@hotmail.com>
2020-10-02 20:48:12 -0500

    add,edit,delele method were updated

--- src/routes/mantener-entrada.js
+++ src/routes/mantener-entrada.js
@@ -4,18 +4,17 @@ const router = express.Router();
 const {isnotlogedin} = require('../lib/out');
 
 router.get('/',isnotlogedin,async(req,res)=>{
-  const entrada = await pool.query("SELECT `entrada`.*, `stock`.`stock_total` FROM `entrada` LEFT JOIN `stock` ON `entrada`.`cod_stock` = `stock`.`cod_stock` WHERE `entrada`.`cod_entrada` != 0 && `entrada`.`ocultar`!=0");
+  const entrada = await pool.query("SELECT `entrada`.*, `stock`.`stock_total` FROM `entrada` LEFT JOIN `stock` ON `entrada`.`cod_stock` = `stock`.`cod_stock` WHERE `entrada`.`codigo` != 0 && `entrada`.`ocultar`!=0");
   res.render('mantener-entrada/mantener-entrada',{entrada});
 })
 
 router.get('/add',isnotlogedin,async(req,res)=>{
-  const entrada = await pool.query("SELECT `entrada`.*, `stock`.`stock_total` FROM `entrada` LEFT JOIN `stock` ON `entrada`.`cod_stock` = `stock`.`cod_stock` WHERE `entrada`.`cod_entrada` != 0 && `entrada`.`ocultar`=0");
-  res.render('mantener-entrada/add',{entrada});
+  res.render('mantener-entrada/add');
 })
 
 router.post('/add',isnotlogedin,async(req,res)=>{
   const {nombre_entrada,precio,stock} = req.body;
-  await pool.query('call insert_entrada(?,?,?)',[nombre_entrada,precio,stock],async(err,resp,fields)=>{
+  await pool.query('call insert_entrada(?,?,?)',[nombre_entrada,precio,stock],async(err,resp)=>{
     if (err) {
       console.log(err);
       req.flash('failure', "No se pudo agregar la entrada" + err);
@@ -29,17 +28,17 @@ router.post('/add',isnotlogedin,async(req,res)=>{
   });
 });
 
-router.get('/edit/:cod_entrada',isnotlogedin,async(req,res)=>{
-  const {cod_entrada} = req.params;
-  const entrada = await pool.query("SELECT `entrada`.*, `stock`.`stock_total` FROM `entrada` LEFT JOIN `stock` ON `entrada`.`cod_stock` = `stock`.`cod_stock` WHERE entrada.cod_entrada=?",[cod_entrada]);
+router.get('/edit/:codigo',isnotlogedin,async(req,res)=>{
+  const {codigo} = req.params;
+  const entrada = await pool.query("SELECT `entrada`.*, `stock`.`stock_total` FROM `entrada` LEFT JOIN `stock` ON `entrada`.`cod_stock` = `stock`.`cod_stock` WHERE entrada.codigo=?",[codigo]);
   res.render('mantener-entrada/edit',{entrada:entrada[0]});
 })
 
-router.post('/edit/:cod_entrada',async(req,res)=>{
-  const {cod_entrada} = req.params;
+router.post('/edit/:codigo', isnotlogedin, async(req,res)=>{
+  const {codigo} = req.params;
   const {nombre_entrada,precio,stock} = req.body;
   
-  await pool.query('call update_entrada(?,?,?,?)',[nombre_entrada,precio,stock,cod_entrada],async(err,resp,fields)=>{
+  await pool.query('call update_entrada(?,?,?,?)',[nombre_entrada,precio,stock,codigo],async(err,resp)=>{
     if (err) {
       req.flash('failure', "No se pudo modificar la entrada" + err);
       res.redirect('/mantener-entrada');
@@ -53,34 +52,18 @@ router.post('/edit/:cod_entrada',async(req,res)=>{
   });
 })
 
-
-router.get('/hide/:cod_entrada',isnotlogedin,async(req,res)=>{
-  const {cod_entrada} = req.params;
+router.get('/delete/:codigo',isnotlogedin,async(req,res)=>{
+  const {codigo} = req.params;
   console.log(req.params)
-  await pool.query(`UPDATE entrada SET ocultar=0 WHERE cod_entrada=${cod_entrada}`, (err, resp, fields) => {
+  await pool.query(`call delete_entrada(?)`,[codigo], (err, resp, fields) => {
+    console.log(resp,fields);
     if (err) {
-        req.flash('failure', "No se pudo ocultar");
+        req.flash('failure', "No se pudo eliminar");
         res.redirect('/mantener-entrada');
         console.log(err)
     }
     else {
-        req.flash('success_delete', 'Ocultado Satisfactoriamente');
-        res.redirect('/mantener-entrada');
-    }
-  });
-});
-
-router.get('/unhide/:cod_entrada',isnotlogedin,async(req,res)=>{
-  const {cod_entrada} = req.params;
-  console.log(req.params)
-  await pool.query(`UPDATE entrada SET ocultar=1 WHERE cod_entrada=${cod_entrada}`, (err, resp, fields) => {
-    if (err) {
-        req.flash('failure', "No se pudo Desocultar");
-        res.redirect('/mantener-entrada');
-        console.log(err)
-    }
-    else {
-        req.flash('success_delete', 'Desocultado Satisfactoriamente');
+        req.flash('success', 'Eliminado Satisfactoriamente');
         res.redirect('/mantener-entrada');
     }
   });
