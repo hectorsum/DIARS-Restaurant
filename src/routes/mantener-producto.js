89ef1883dd026e82b07c0c5ff148f0059b6934ca
Hector Herrera <hectorsum81@hotmail.com>
2020-10-04 23:17:20 -0500

    codigo updated by cod_

--- src/routes/mantener-producto.js
+++ src/routes/mantener-producto.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {isnotlogedin}=require('../lib/out');
 
 router.get('/',isnotlogedin,async(req,res)=>{
-  const producto = await pool.query("SELECT `producto`.*, `stock`.`stock_total` FROM `producto` LEFT JOIN `stock` ON `producto`.`cod_stock` = `stock`.`cod_stock` WHERE `producto`.`codigo` != 0 && `producto`.`ocultar`!=0");
+  const producto = await pool.query("SELECT `producto`.*, `stock`.`stock_total` FROM `producto` LEFT JOIN `stock` ON `producto`.`cod_stock` = `stock`.`cod_stock` WHERE `producto`.`cod_prod` != 0 && `producto`.`ocultar`!=0");
   res.render('mantener-producto/mantener-producto',{producto});
 });
 
@@ -27,17 +27,17 @@ router.post('/add',isnotlogedin,async(req,res)=>{
   });
 });
 
-router.get('/edit/:codigo',isnotlogedin,async(req,res)=>{
-  const {codigo} = req.params;
-  const producto = await pool.query("SELECT `producto`.*, `stock`.`stock_total` FROM `producto` LEFT JOIN `stock` ON `producto`.`cod_stock` = `stock`.`cod_stock` WHERE producto.codigo=?",[codigo]);
+router.get('/edit/:cod_prod',isnotlogedin,async(req,res)=>{
+  const {cod_prod} = req.params;
+  const producto = await pool.query("SELECT `producto`.*, `stock`.`stock_total` FROM `producto` LEFT JOIN `stock` ON `producto`.`cod_stock` = `stock`.`cod_stock` WHERE producto.cod_prod=?",[cod_prod]);
   res.render('mantener-producto/edit',{producto:producto[0]});
 })
 
-router.post('/edit/:codigo',async(req,res)=>{
-  const {codigo} = req.params;
+router.post('/edit/:cod_prod',async(req,res)=>{
+  const {cod_prod} = req.params;
   const {nombre_producto,precio,stock} = req.body;
   
-  await pool.query('call update_product(?,?,?,?)',[nombre_producto,precio,stock,codigo],async(err,resp)=>{
+  await pool.query('call update_product(?,?,?,?)',[nombre_producto,precio,stock,cod_prod],async(err,resp)=>{
     if (err) {
       req.flash('failure', "No se pudo agregar el producto" + err);
       res.redirect('/mantener-producto');
@@ -52,10 +52,10 @@ router.post('/edit/:codigo',async(req,res)=>{
 })
 
 
-router.get('/delete/:codigo',isnotlogedin,async(req,res)=>{
-  const {codigo} = req.params;
+router.get('/delete/:cod_prod',isnotlogedin,async(req,res)=>{
+  const {cod_prod} = req.params;
   console.log(req.params)
-  await pool.query(`call delete_producto(?)`,[codigo], (err, resp, fields) => {
+  await pool.query(`call delete_producto(?)`,[cod_prod], (err, resp, fields) => {
     if (err) {
         req.flash('failure', "No se pudo eliminar");
         res.redirect('/mantener-producto');
--- src/routes/mantener-segundo.js
+++ src/routes/mantener-segundo.js
@@ -4,7 +4,7 @@ const {isnotlogedin} = require('../lib/out');
 const router = express.Router();
 
 router.get('/',isnotlogedin, async(req,res)=>{
-  const segundo = await pool.query("SELECT `segundo`.*, `stock`.`stock_total` FROM `segundo` LEFT JOIN `stock` ON `segundo`.`cod_stock` = `stock`.`cod_stock` WHERE `segundo`.`codigo` != 0 && `segundo`.`ocultar`!=0");
+  const segundo = await pool.query("SELECT `segundo`.*, `stock`.`stock_total` FROM `segundo` LEFT JOIN `stock` ON `segundo`.`cod_stock` = `stock`.`cod_stock` WHERE `segundo`.`cod_segundo` != 0 && `segundo`.`ocultar`!=0");
   res.render('mantener-segundo/mantener-segundo',{segundo});
 })
 
@@ -25,17 +25,17 @@ router.post('/add',isnotlogedin,async(req,res)=>{
   })
 })
 
-router.get('/edit/:codigo',isnotlogedin,async(req,res)=>{
-  const {codigo} = req.params;
-  const segundo = await pool.query("SELECT `segundo`.*, `stock`.`stock_total` FROM `segundo` LEFT JOIN `stock` ON `segundo`.`cod_stock` = `stock`.`cod_stock` WHERE segundo.codigo=?",[codigo]);
+router.get('/edit/:cod_segundo',isnotlogedin,async(req,res)=>{
+  const {cod_segundo} = req.params;
+  const segundo = await pool.query("SELECT `segundo`.*, `stock`.`stock_total` FROM `segundo` LEFT JOIN `stock` ON `segundo`.`cod_stock` = `stock`.`cod_stock` WHERE segundo.cod_segundo=?",[cod_segundo]);
   res.render('mantener-segundo/edit',{segundo:segundo[0]});
 })
 
-router.post('/edit/:codigo',async(req,res)=>{
-  const {codigo} = req.params;
+router.post('/edit/:cod_segundo',async(req,res)=>{
+  const {cod_segundo} = req.params;
   const {nombre_segundo,precio,stock} = req.body;
   
-  await pool.query('call update_segundo(?,?,?,?)',[nombre_segundo,precio,stock,codigo],async(err,resp)=>{
+  await pool.query('call update_segundo(?,?,?,?)',[nombre_segundo,precio,stock,cod_segundo],async(err,resp)=>{
     if (err) {
       req.flash('failure', "No se pudo agregar el segundo" + err);
       res.redirect('/mantener-segundo');
@@ -49,9 +49,9 @@ router.post('/edit/:codigo',async(req,res)=>{
   });
 })
 
-router.get('/delete/:codigo',isnotlogedin,async(req,res)=>{
-  const {codigo} = req.params;
-  await pool.query(`call delete_segundo(?)`,[codigo], (err, resp) => {
+router.get('/delete/:cod_segundo',isnotlogedin,async(req,res)=>{
+  const {cod_segundo} = req.params;
+  await pool.query(`call delete_segundo(?)`,[cod_segundo], (err, resp) => {
     if (err) {
         req.flash('failure', "No se pudo eliminar");
         res.redirect('/mantener-segundo');
