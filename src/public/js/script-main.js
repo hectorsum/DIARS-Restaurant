c753fc98ff5f2fb6be390a573d9501ced09db2f3
Hector Herrera <hectorsum81@hotmail.com>
2020-10-09 23:56:13 -0500

    onChange functions from registrar-comanda were just removed

--- src/public/js/script-main.js
+++ src/public/js/script-main.js
@@ -36,9 +36,9 @@ try {
 }
 
 //todo: Setting time and date to input from Generar-Cuenta
-const date_input = document.getElementById('date');
+const date_input = document.querySelector('#date');
 
-date_input.addEventListener('load',setInterval(clock,1000));
+date_input.addEventListener('DOMContentLoaded',clock);
 
 function clock(){
   var date = new Date();
@@ -49,73 +49,15 @@ function clock(){
   var min = date.getMinutes();
   var sec = date.getSeconds();
   
-  //*Validations date
+  //*Validations date (conditional - ternary operator)
   var sec = (sec<10) ? `0${sec}` : sec;
   var min = (min<10) ? `0${min}` : min;
   var hour = (hour<10) ? `0${hour}`: hour;
-  var month = (month<10) ? `0${month+1}` : month+1;
-
+  //var month = (month<=10) ? `0${month+1}` : `${month+1}`; (Not working!!) 
+  var month = (month.length===1) ? `0${month+1}` : `${month+1}`;
   const time = document.getElementById('date').placeholder = `${day}/${month}/${year} ${hour}:${min}:${sec}`
   return time;
 }
-
-function changedSegundo(){
-  const nombre_segundo = document.getElementById('nombre_segundo');
-  //* Getting integer value from selected tag
-  const opc_nombre_segundo = nombre_segundo.options[nombre_segundo.selectedIndex].value;
-  const cant_segundo = document.getElementById('cant_segundo');
-
-  switch(opc_nombre_segundo){
-    case '0':
-      cant_segundo.setAttribute('readonly',true);
-      cant_segundo.setAttribute('value','0');
-      break;
-    default:
-      cant_segundo.removeAttribute('readonly');
-      cant_segundo.setAttribute('value','1');
-      break;
-  }
-}
-
-function changedEntrada(){
-  const nombre_entrada = document.getElementById('nombre_entrada');
-  //* Getting integer value from selected tag
-  const opc_nombre_entrada = nombre_entrada.options[nombre_entrada.selectedIndex].value;
-  const cant_entrada = document.getElementById('cant_entrada');
-
-  switch(opc_nombre_entrada){
-    case '0':
-      cant_entrada.setAttribute('readonly',true);
-      cant_entrada.setAttribute('value','0');
-      break;
-    default:
-      cant_entrada.removeAttribute('readonly');
-      cant_entrada.setAttribute('value','1');
-      break;
-  }
-}
-
-function changedProducto(){
-  const nombre_producto = document.getElementById('nombre_producto');
-  //* Getting integer value from selected tag
-  const opc_nombre_producto = nombre_producto.options[nombre_producto.selectedIndex].value;
-  const cant_producto = document.getElementById('cant_producto');
-
-  switch(opc_nombre_producto){
-    case '0':
-      cant_producto.setAttribute('readonly',true);
-      cant_producto.setAttribute('value','0');
-      break;
-    default:
-      cant_producto.removeAttribute('readonly');
-      cant_producto.setAttribute('value','1');
-      break;
-  }
-}
-
-window.addEventListener('change',changedSegundo);
-window.addEventListener('change',changedEntrada);
-window.addEventListener('change',changedProducto);
 
 function popup_add_producto(){
   document.querySelector('.mantener-productos .popup-add-producto').style.display = 'flex';
