9de29bebd26d7538356a71e7b6cff3fc24cd4f9b
Hector Herrera <hectorsum81@hotmail.com>
2020-10-22 23:44:27 -0500

    database query updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database');
 
-
-router.get('/',(req,res)=>{
+router.get('/',async (req,res)=>{
   // res.send('HOME')
   // Aiming layouts/index for webpage
-  res.render('web/index',{layout:'index'})
+  const carta = await pool.query("SELECT * FROM carta WHERE categoria LIKE 'producto' and estado=1")
+  res.render('web/index',{carta,layout:'index'});
 });
 
 
