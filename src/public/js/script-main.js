c4c4cf9748ecb81b3e2ad41aca31b7fce930c5c3
Hector Herrera <hectorsum81@hotmail.com>
2020-10-29 23:08:48 -0500

    some jquery code added

--- src/public/js/script-main.js
+++ src/public/js/script-main.js
@@ -66,11 +66,24 @@ $(document).ready(function() {
       },
       scrollY:        200,
       scrollCollapse: true,
-      paging:         true
+      paging:         true,
+      responsive: {
+        breakpoints: [
+          {name: 'bigdesktop', width: Infinity},
+          {name: 'meddesktop', width: 1480},
+          {name: 'smalldesktop', width: 1280},
+          {name: 'medium', width: 1188},
+          {name: 'tabletl', width: 1024},
+          {name: 'btwtabllandp', width: 848},
+          {name: 'tabletp', width: 768},
+          {name: 'mobilel', width: 480},
+          {name: 'mobilep', width: 320}
+        ]
+      }
   } );
 
   // Apply a search to the second table for the demo
-  //$('#myTable2').DataTable().search( 'New York' ).draw();
+  //$('#myTable2').DataTable().search( 'A' ).draw();
 } );
 
 $(document).ready( function () {
@@ -96,3 +109,48 @@ $(document).ready( function () {
     }
   });
 });
+
+
+//Set worker-add user
+try {
+  //Looking for button check
+    $(".popup-add-producto button").click(function () {
+        console.log('clicked')
+        var row = $(this).closest("tr");
+        document.getElementById('empleado').value = row.find("td:eq(1)").text();
+        document.getElementById('dni').value = row.find("td:eq(2)").text();
+        document.getElementById('rol').value = row.find("td:eq(3)").text();
+        
+        document.getElementById('usuario').removeAttribute('readonly');
+        document.getElementById('usuario').setAttribute('required',true);
+        document.getElementById('password').removeAttribute('readonly');
+        document.getElementById('password').setAttribute('required',true);
+        
+        document.getElementById('btnAgregarUsuario').removeAttribute('disabled');
+
+        document.querySelector(".mantener-productos .popup-add-producto").style.display = "none";
+    });
+} catch (error) {
+}
+
+
+try {
+  //Looking for button check
+    $("#search_comanda button").click(function () {
+        console.log('clicked')
+        var row = $(this).closest("tr");
+        document.getElementById('empleado').value = row.find("td:eq(1)").text();
+        document.getElementById('dni').value = row.find("td:eq(2)").text();
+        document.getElementById('rol').value = row.find("td:eq(3)").text();
+        
+        document.getElementById('usuario').removeAttribute('readonly');
+        document.getElementById('usuario').setAttribute('required',true);
+        document.getElementById('password').removeAttribute('readonly');
+        document.getElementById('password').setAttribute('required',true);
+        
+        document.getElementById('btnAgregarUsuario').removeAttribute('disabled');
+
+        document.querySelector(".mantener-productos .popup-add-producto").style.display = "none";
+    });
+} catch (error) {
+}
