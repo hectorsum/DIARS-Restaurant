1dadc4b0d0b084436daf8e904927857ec7760919
Hector Herrera <hectorsum81@hotmail.com>
2020-11-01 02:29:50 -0500

    some queries were added

--- src/routes/consultar-cliente.js
+++ src/routes/consultar-cliente.js
@@ -4,10 +4,9 @@ const {isnotlogedin} = require('../lib/out');
 const pool = require('../database');
 
 router.get('/consultar-cliente',isnotlogedin,async(req,res)=>{
-  const entrada = await pool.query("SELECT * FROM carta WHERE categoria LIKE 'entrada' and estado=1");
-  const segundo = await pool.query("SELECT * FROM carta WHERE categoria LIKE 'segundo' and estado=1");
-  console.log(segundo);
-  res.render('consultar-cliente/consultar-cliente',{entrada,segundo});
+  const contribuyente = await pool.query("SELECT * FROM contribuyente");
+  const cliente = await pool.query("SELECT * FROM cliente");
+  res.render('consultar-cliente/consultar-cliente',{contribuyente,cliente});
 })
 
 
