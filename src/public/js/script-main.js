abda9ceedc847bfc40542265e8c3168485a5f18e
Hector Herrera <hectorsum81@hotmail.com>
2020-11-01 02:29:06 -0500

    some buttons from dataTable library were added

--- src/public/js/script-main.js
+++ src/public/js/script-main.js
@@ -78,12 +78,29 @@ $(document).ready(function() {
           {name: 'tabletp', width: 768},
           {name: 'mobilel', width: 480},
           {name: 'mobilep', width: 320}
-        ]
-      }
+        ]},
+      dom: 'Bfrtilp',
+      buttons:[
+        {
+          extend: 'excelHtml5',
+          text:'<i class="fas fa-file-excel"></i>',
+          titleAttr: 'Exportar a Excel',
+          className: 'btn btn-success'
+        },
+        {
+          extend: 'pdfHtml5',
+          text:'<i class="fas fa-file-pdf"></i>',
+          titleAttr: 'Exportar a PDF',
+          className: 'btn btn-danger'
+        },
+        {
+          extend: 'print',
+          text:'<i class="fas fa-print"></i>',
+          titleAttr: 'Imprimir',
+          className: 'btn btn-info'
+        },
+      ]
   } );
-
-  // Apply a search to the second table for the demo
-  //$('#myTable2').DataTable().search( 'A' ).draw();
 } );
 
 $(document).ready( function () {
