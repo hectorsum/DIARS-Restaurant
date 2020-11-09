c24d8627184de368df2839a1eada0f3c343483c2
Diego <csjoin99@gmail.com>
2020-11-09 13:10:04 -0500

    Se modifico para que guarde la imagen con la extensión original

--- src/lib/multer.js
+++ src/lib/multer.js
@@ -4,21 +4,22 @@ const path = require('path');
 const uuid = require('uuid');
 
 const storage = multer.diskStorage({
-  destination: path.join(__dirname,'../public/uploads'),
-  filename: (req,file,cb) =>{
-    var date = new Date();
-    var dd = String(date.getDate()).padStart(2,'0');
-    var mm = String(date.getMonth()+1).padStart(2,'0');
-    var yyyy = date.getFullYear();
-    var fullDate = dd+ '-' + mm + '-' + yyyy;
-    file.originalname = uuid.v4()+'-'+fullDate+'.png';
-    cb(null,file.originalname);
-  }
+    destination: path.join(__dirname, '../public/uploads'),
+    filename: (req, file, cb) => {
+        let extArray = file.mimetype.split("/");
+        var date = new Date();
+        var dd = String(date.getDate()).padStart(2, '0');
+        var mm = String(date.getMonth() + 1).padStart(2, '0');
+        var yyyy = date.getFullYear();
+        var fullDate = dd + '-' + mm + '-' + yyyy;
+        file.originalname = uuid.v4() + '-' + fullDate + '.' + extArray[1];
+        cb(null, file.originalname);
+    }
 });
 
 const uploadImage = multer({
-  storage,
-  limit:{ fileSize: 1000000 }
+    storage,
+    limit: { fileSize: 1000000 }
 }).single('image');
 
 module.exports = uploadImage;
