6bcad0eccb2b6f82a6964d55931c6bd248c13021
Hector Herrera <hectorsum81@hotmail.com>
2020-11-09 01:43:00 -0500

    some queries removeds

--- src/routes/generar-cuenta.js
+++ src/routes/generar-cuenta.js
@@ -9,14 +9,12 @@ router.get('/',isnotlogedin,async(req,res)=>{
   
   //*Querying tables
   const venta = await pool.query("SELECT `venta`.*, `tipo_pago`.`*`, `tipo_comprobante`.`nombre_comprobante` FROM `venta` LEFT JOIN `tipo_pago` ON `venta`.`cod_tipo_pago` = `tipo_pago`.`cod_tipo_pago` LEFT JOIN `tipo_comprobante` ON `venta`.`cod_tipo_comprobante` = `tipo_comprobante`.`cod_tipo_comprobante` WHERE DATE(fecha_venta) LIKE CURDATE() and estado_pago='cancelado'");
-  const entrada = await pool.query('SELECT * FROM entrada')
-  const segundo = await pool.query('SELECT * FROM segundo')
-  const producto = await pool.query('SELECT * FROM producto')
   const tipo_pago = await pool.query('SELECT * FROM tipo_pago')
   const tipo_comprobante = await pool.query('SELECT * FROM tipo_comprobante')
   const usuario_emp = await pool.query("SELECT `usuario_emp`.`cod_emp`, `empleado`.`nombres`, `empleado`.`apellidos` FROM `usuario_emp` LEFT JOIN `empleado` ON `usuario_emp`.`cod_emp` = `empleado`.`cod_emp`")
   const comanda = await pool.query("SELECT * FROM venta WHERE estado=1 ORDER BY cod_ven DESC");
-  res.render('generar-cuenta/generar-cuenta',{venta,entrada,segundo,producto,tipo_pago,usuario_emp,tipo_comprobante,comanda})
+  
+  res.render('generar-cuenta/generar-cuenta',{venta,tipo_pago,usuario_emp,tipo_comprobante,comanda})
 })
 
 router.post('/', isnotlogedin,async(req,res)=>{
