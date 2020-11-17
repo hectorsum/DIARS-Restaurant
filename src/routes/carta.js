62d5e546cdf01d7f3671b86be9fcee0c9feb53d2
Hector Herrera <hectorsum81@hotmail.com>
2020-11-17 03:05:39 -0500

    C:/Program Files/Git/get /post methods were added

--- src/routes/carta.js
+++ src/routes/carta.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database');
 
+router.get('/',async(req,res)=>{
+  const segundo = await pool.query('SELECT * from carta WHERE categoria="segundo" and estado=1')
+  console.log(segundo);
+  const entrada = await pool.query('SELECT * from carta WHERE categoria="entrada" and estado=1')
+  console.log(segundo);
+  const producto = await pool.query('SELECT * from carta WHERE categoria="producto" and estado=1')
+  console.log(segundo);
+  res.render('web/carta',{layout:'index',segundo,entrada,producto});
+})
+router.get('/detalle-item/:cod_carta',async(req,res)=>{
+  const {cod_carta} = req.params;
+  const carta = await pool.query("SELECT * FROM carta WHERE cod_carta=?",[cod_carta]);
+  console.log(carta)
+  res.render('web/detalle-item',{layout:'index',carta:carta[0]});
+});
+router.post('/detalle-item/:cod_carta',async(req,res)=>{
+  const cod_carta = req.params;
+
+});
 
 module.exports = router;
