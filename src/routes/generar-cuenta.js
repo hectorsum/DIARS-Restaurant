8582099054ca68399195a33ffa9698997dc5f7ab
Hector Herrera <hectorsum81@hotmail.com>
2020-11-21 02:07:26 -0500

    some queries updated

--- src/routes/generar-cuenta.js
+++ src/routes/generar-cuenta.js
@@ -8,14 +8,14 @@ const pdf = require('../lib/puppeteer');
 router.get('/', isnotlogedin, async(req, res) => {
 
     //*Querying tables
-    const venta = await pool.query("SELECT venta.*, tipo_pago.*, tipo_comprobante.nombre_comprobante FROM venta LEFT JOIN tipo_pago ON venta.cod_tipo_pago = tipo_pago.cod_tipo_pago LEFT JOIN tipo_comprobante ON venta.cod_tipo_comprobante = tipo_comprobante.cod_tipo_comprobante WHERE DATE(fecha_venta) LIKE CURDATE() and estado_pago='cancelado'");
+    const venta = await pool.query("SELECT venta.*, tipo_pago.*, tipo_comprobante.nombre_comprobante FROM venta LEFT JOIN tipo_pago ON venta.cod_tipo_pago = tipo_pago.cod_tipo_pago LEFT JOIN tipo_comprobante ON venta.cod_tipo_comprobante = tipo_comprobante.cod_tipo_comprobante WHERE DATE(fecha_venta) LIKE CURDATE() and estado_pago='cancelado' and tipo_venta='local'");
     /* const entrada = await pool.query('SELECT * FROM entrada') */
     /* const segundo = await pool.query('SELECT * FROM segundo') */
     /* const producto = await pool.query('SELECT * FROM producto') */
     const tipo_pago = await pool.query('SELECT * FROM tipo_pago')
     const tipo_comprobante = await pool.query('SELECT * FROM tipo_comprobante')
     const usuario_emp = await pool.query("SELECT `usuario_emp`.`cod_emp`, `empleado`.`nombres`, `empleado`.`apellidos` FROM `usuario_emp` LEFT JOIN `empleado` ON `usuario_emp`.`cod_emp` = `empleado`.`cod_emp`")
-    const comanda = await pool.query("SELECT * FROM venta WHERE estado=1 ORDER BY cod_ven DESC");
+    const comanda = await pool.query("SELECT * FROM venta WHERE estado=1 and tipo_venta!='delivery' ORDER BY cod_ven DESC");
     res.render('generar-cuenta/generar-cuenta', { venta, tipo_pago, usuario_emp, tipo_comprobante, comanda })
 })
 
@@ -44,16 +44,15 @@ router.post('/edit/:cod_ven', isnotlogedin, async(req, res) => {
 
 //pdf
 router.get('/print/:cod_ven', isnotlogedin, async(req, res) => {
-    try {
-        const { cod_ven } = req.params;
-        var npdf = await pdf.boleta(cod_ven);
-        console.log('ERROR HERE: ', npdf)
-        res.contentType("application/pdf");
-        res.send(npdf);
-    } catch (e) {
-        console.log(e);
-    }
-
+  try {
+      const { cod_ven } = req.params;
+      var npdf = await pdf.boleta(cod_ven);
+      console.log('ERROR HERE: ', npdf)
+      res.contentType("application/pdf");
+      res.send(npdf);
+  } catch (e) {
+      console.log(e);
+  }
 });
 
 module.exports = router;
