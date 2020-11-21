92c072b9492b15d4eaaec837a7945344fcb1bc45
Hector Herrera <hectorsum81@hotmail.com>
2020-11-21 17:58:22 -0500

    new query added

--- src/routes/equipo.js
+++ src/routes/equipo.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-router.get('/equipo',(req,res)=>{
-  res.render('web/equipo',{layout:'index'})
+router.get('/equipo',async(req,res)=>{
+  const equipo = await pool.query("SELECT `empleado`.*, `rol`.`rol_nombre` FROM `empleado` LEFT JOIN `rol` ON `empleado`.`cod_rol` = `rol`.`cod_rol` WHERE rol.rol_nombre='chef'")
+  console.log(equipo)
+  res.render('web/equipo',{layout:'index',equipo});
 })
 
 module.exports = router;
