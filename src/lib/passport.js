90608adbd117b3cb1737cf34b24244d9fb95d469
Hector Herrera <hectorsum81@hotmail.com>
2020-11-22 20:18:02 -0500

    gravatar added

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const localstrategy = require('passport-local').Strategy;
 const pool = require('../database');
 const helpers = require('../lib/helpers');
+const gravatar = require('gravatar');
 
 passport.use('local.signin', new localstrategy({
   usernameField:'usuario',
@@ -27,6 +28,15 @@ passport.serializeUser((user,done)=>{
 })
 //* Allows us to use empleados columns with user.<nombres>
 passport.deserializeUser(async(id,done)=>{
-  const rows = await pool.query("SELECT `usuario_emp`.*, `empleado`.*, `rol`.`*` FROM `usuario_emp` LEFT JOIN `empleado` ON `usuario_emp`.`cod_emp` = `empleado`.`cod_emp` LEFT JOIN `rol` ON `empleado`.`cod_rol` = `rol`.`cod_rol` WHERE `usuario_emp`.`cod_usuario_emp` = ?",[id]);
+  const rows = await pool.query("SELECT `usuario_emp`.*,`empleado`.`photo`, `empleado`.*, `rol`.`*` FROM `usuario_emp` LEFT JOIN `empleado` ON `usuario_emp`.`cod_emp` = `empleado`.`cod_emp` LEFT JOIN `rol` ON `empleado`.`cod_rol` = `rol`.`cod_rol` WHERE `usuario_emp`.`cod_usuario_emp` = ?",[id]);
+  for (i = 0; i < rows.length; i++) {
+    if (!rows[i].photo) {
+        const url = gravatar.url(rows[i].email, { s: '100', r: 'x', d: 'mp' }, false);
+        rows[i].photo = url
+    } else {
+      rows[i].photo = 'uploads/' + rows[i].photo
+    }
+  }
+  console.log(rows);
   done(null,rows[0]);
 })
