a8490cb8b4f315e4c9763f3bc14a5235f2ffd09d
Hector Herrera <hectorsum81@hotmail.com>
2020-12-08 11:41:41 -0500

    new queries added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const pool = require('../database');
 
 router.get('/',async (req,res)=>{
-  // res.send('HOME')
-  // Aiming layouts/index for webpage
-  const carta = await pool.query("SELECT * FROM carta WHERE categoria LIKE 'producto' and estado=1")
-  res.render('web/index',{carta,layout:'index'});
+  //* Aiming layouts/index for webpage
+  const carta = await pool.query("SELECT * FROM carta WHERE categoria LIKE 'producto' and estado=1");
+  const equipo = await pool.query("SELECT `empleado`.*, `rol`.`rol_nombre` FROM `empleado` LEFT JOIN `rol` ON `empleado`.`cod_rol` = `rol`.`cod_rol` WHERE rol.rol_nombre='chef'");
+  console.log(equipo);
+  res.render('web/index',{equipo,carta,layout:'index'});
 });
 
 
